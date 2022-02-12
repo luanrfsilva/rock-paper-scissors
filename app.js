346922326a9874d3de691f0ea9763fabb301c985
Luã Silva <luanrfsilva@gmail.com>
2022-02-12 17:27:15 -0300

    Add game logic and update some styles

---
+++ app.js
@@ -0,0 +1,80 @@
+let user_score = 0
+let computer_score = 0
+let total_games = 0
+
+const user_score_element = document.getElementById('user-score')
+const computer_score_element = document.getElementById('comp-score')
+const total_games_element = document.getElementById('total-games')
+const result_element = document.getElementsByClassName('result')[0]
+const reset_game_element = document.getElementById('reset-game')
+
+const rock_element = document.getElementById('rock')
+const paper_element = document.getElementById('paper')
+const scissors_element = document.getElementById('scissors')
+
+function computer_choice() {
+  const choices = ['rock', 'paper', 'scissors']
+  const choice = choices[Math.floor(Math.random() * 3)]
+  return choice
+}
+
+function win() {
+  user_score++
+  user_score_element.innerHTML = user_score
+  result_element.innerHTML = '<p>You win!</p>'
+}
+
+function lose() {
+  computer_score++
+  computer_score_element.innerHTML = computer_score
+  result_element.innerHTML = '<p>You lose!</p>'
+}
+
+function draw() {
+  result_element.innerHTML = '<p>Its a draw!</p>'
+}
+
+function game(user_choice) {
+  total_games++
+  total_games_element.innerHTML = total_games
+  switch (`${user_choice}_${computer_choice()}`) {
+    case 'rock_scissors':
+    case 'scissors_paper':
+    case 'paper_rock':
+      win()
+      break
+    case 'scissors_rock':
+    case 'paper_scissors':
+    case 'rock_paper':
+      lose()
+      break
+    default:
+      draw()
+      break
+  }
+}
+
+function main() {
+  rock_element.addEventListener('click', () => {
+    game('rock')
+  })
+  paper_element.addEventListener('click', () => {
+    game('paper')
+  })
+  scissors_element.addEventListener('click', () => {
+    game('scissors')
+  })
+
+  reset_game_element.addEventListener('click', () => {
+    result_element.innerHTML = '<p>Start the game!</p>'
+    user_score_element.innerHTML = 0
+    computer_score_element.innerHTML = 0
+    total_games_element.innerHTML = 0
+
+    user_score = 0
+    computer_score = 0
+    total_games = 0
+  })
+}
+
+main()
